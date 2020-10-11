794083aba3b818ad58480f4e6bdf58980d876a62
local <102837790@qq.com>
2020-10-11 18:27:26 +0800

    数据类型和简单计算

--- demo.py
+++ demo.py
@@ -1,1 +1,39 @@
-print("你好")
+print('你好'+"带我玩"*3,end="111")
+print(123)
+# print(2.333)
+"""
+print((2+2-1*1/0.5)*2)
+print(True,False)
+print(1>3)
+print(1<3)
+print(1==2 or 1<3)
+print(1==2 and 1<3)
+print(None)
+print(())
+print([])
+print({})"""
+
+name='张三' 
+print(name)
+
+#运算输出
+res= (1+2+4+5*2/(4*2))%3
+print(res)
+
+#获取内容并切换成小数进行运算
+# a=input("输入:")
+# b=input("输入2")
+# d=int(input("输入3"))
+# print(float(a)+float(b)+d)
+# c=type(b)
+# print(c)
+
+#获取字段长度
+# a=len("dasdadsdasd")
+# print(a)
+
+#判断输入字符串是奇数还是偶数
+print("True为奇,False为偶")
+a=len(input("请输入"))
+c=int(a)%2
+print(c>0)
